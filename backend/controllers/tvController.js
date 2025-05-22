e494916cf5eba50686a91552529bd5cb8756c83b
miguel-bomfim-gsoul <miguel.bomfim@gsoul.com.br>
2025-05-22 15:58:46 -0300

    fix: improve getallTvs method

--- backend/controllers/tvController.js
+++ backend/controllers/tvController.js
@@ -11,9 +11,43 @@ export async function addTv (req, res){
 }
 
 export async function getallTVs (req, res) {
-    db.query('SELECT * FROM tvs', (err, results) => {
-        if (err) return res.staturs(500).json({ error: err });
-        res.json(results);
+    const query = `
+        SELECT
+            tvs.id AS tv_id,
+            tvs.name,
+            media.id AS image_id,
+            media.url_image,
+            media.media_order 
+        FROM tvs
+        LEFT JOIN
+            media ON media.tv_id = tvs.id
+        ORDER BY
+            tvs.id, media.media_order
+    `
+    db.query(query, (err, results) => {
+        if (err) return res.status(500).json({ error: err });
+
+        const tvs = {};
+        results.forEach((row) => {
+            const { tv_id, name, image_id, url_image, media_order } = row;
+
+            if (!tvs[tv_id]) {
+                tvs[tv_id] = {
+                    id: tv_id,
+                    name,
+                    images: []
+                };
+            }
+
+            if (image_id) {
+                tvs[tv_id].images.push({
+                    id: image_id,
+                    url_image,
+                    media_order
+                });
+            }
+        });
+        res.json(Object.values(tvs));
     })
 }
 
