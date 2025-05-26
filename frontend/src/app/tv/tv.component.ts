a46fd9d3f94922383c7d90ae724893cee2d24c2c
miguel-bomfim-gsoul <miguel.bomfim@gsoul.com.br>
2025-05-26 09:09:17 -0300

    feat: implement image carousel tv sceen with mocked values

--- frontend/src/app/tv/tv.component.ts
+++ frontend/src/app/tv/tv.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-tv',
@@ -6,4 +6,29 @@ import { Component } from '@angular/core';
   templateUrl: './tv.component.html',
   styleUrl: './tv.component.css'
 })
-export class TvComponent {}
+export class TvComponent implements OnInit, OnDestroy {
+  images: string[] = [
+    'https://picsum.photos/id/1015/1920/1080',
+    'https://picsum.photos/id/1016/1920/1080',
+    'https://picsum.photos/id/1018/1920/1080',
+    'https://picsum.photos/id/1019/1920/1080',
+    'https://picsum.photos/id/1020/1920/1080'
+  ];
+  currentIndex = 0;
+  intervalId: any;
+  delay = 10000; // 5 seconds
+
+  ngOnInit() {
+    this.startCarousel();
+  }
+
+  startCarousel() {
+    this.intervalId = setInterval(() => {
+      this.currentIndex = (this.currentIndex + 1) % this.images.length;
+    }, this.delay);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.intervalId);
+  }
+}
